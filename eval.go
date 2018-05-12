ab20842ec91f66f3966b42c099f76bbe68a5eb22
Nick Santos <nicholas.j.santos@gmail.com>
2018-05-12 16:56:09 -0400

    value: Add a Sliceable interface for operands of the [] operator. Fixes #104

--- eval.go
+++ eval.go
@@ -837,19 +837,6 @@ func slice(x, lo, hi, step_ Value) (Value, error) {
 		if end < start {
 			end = start // => empty result
 		}
-
-		if step == 1 {
-			// common case: simple subsequence
-			switch x := x.(type) {
-			case String:
-				return String(x[start:end]), nil
-			case *List:
-				elems := append([]Value{}, x.elems[start:end]...)
-				return NewList(elems), nil
-			case Tuple:
-				return x[start:end], nil
-			}
-		}
 	} else {
 		// negative stride
 		// default indices are effectively [n-1:-1], though to
@@ -876,31 +863,11 @@ func slice(x, lo, hi, step_ Value) (Value, error) {
 		}
 	}
 
-	// For positive strides, the loop condition is i < end.
-	// For negative strides, the loop condition is i > end.
-	sign := signum(step)
-	switch x := x.(type) {
-	case String:
-		var str []byte
-		for i := start; signum(end-i) == sign; i += step {
-			str = append(str, x[i])
-		}
-		return String(str), nil
-	case *List:
-		var list []Value
-		for i := start; signum(end-i) == sign; i += step {
-			list = append(list, x.elems[i])
-		}
-		return NewList(list), nil
-	case Tuple:
-		var tuple Tuple
-		for i := start; signum(end-i) == sign; i += step {
-			tuple = append(tuple, x[i])
-		}
-		return tuple, nil
+	sliceable, ok := x.(Sliceable)
+	if !ok {
+		return nil, fmt.Errorf("invalid slice operand %s", x.Type())
 	}
-
-	return nil, fmt.Errorf("invalid slice operand %s", x.Type())
+	return sliceable.Slice(start, end, step), nil
 }
 
 // From Hacker's Delight, section 2.8.
